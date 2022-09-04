30dec65ab14f416a051738513c12b7ee52e6f20f
unknown <esteban.quirozv@gmail.com>
2022-09-04 17:03:21 -0500

    test unitarios de image.service

--- src/app/image.service.spec.ts
+++ src/app/image.service.spec.ts
@@ -6,11 +6,29 @@ describe('ImageService', () => {
   let service: ImageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({providers: [ImageService]});
     service = TestBed.inject(ImageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+  describe('getImages', () => {
+    it('debe retornar todas las imagenes', () => {
+      const imagenes = service.getImages();
+      expect(imagenes.length).toEqual(5);
+    });
+  });
+
+  describe('getImage', () => {
+    it('debe retornar la imagen con id si existe', () => {
+      const imagene = service.getImage(1);
+      expect(imagene.brand).toEqual('perro');
+    });
+
+    it('debe retornar indefinido si se busca una imagen con id que NO existe', () => {
+      const imagene = service.getImage(100);
+      expect(imagene).toEqual(undefined);
+    });
   });
 });
